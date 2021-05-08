9513ca7123e68ff87d206d510c3bb7aa4ae59dae
zann <naim_khsn@live.co.uk>
2021-05-08 10:30:44 -0400

    initial commit

---
+++ src/main/java/com/peoplentech/selenium/DriverLaunch1.java
@@ -0,0 +1,30 @@
+package com.peoplentech.selenium;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverLaunch1 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // setup the env property
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        // created obj of chromedriver
+        WebDriver driver = new ChromeDriver();
+
+        // open the browser
+        driver.get("https://www.google.com");
+
+        // wait
+        Thread.sleep(5000);
+
+        // quit the browser
+        driver.quit();
+
+
+    }
+
+
+}
