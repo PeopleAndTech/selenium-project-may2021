b8d87ed524636329a8898d47209b845c0f870f66
zann <naim_khsn@live.co.uk>
2021-05-09 11:04:46 -0400

    added logger & selenium tests

---
+++ src/main/java/com/peoplentech/selenium/TestBase.java
@@ -0,0 +1,64 @@
+package com.peoplentech.selenium;
+
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    private static final Logger logger = Logger.getLogger(TestBase.class);
+    public static WebDriver driver;
+
+    public static void setupBrowser(String browserName, String url) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+            driver = new ChromeDriver();
+        } else {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+            driver = new FirefoxDriver();
+        }
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(url);
+    }
+
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeBrowser() {
+        waitFor(5);
+        driver.quit();
+    }
+
+    public static void clickOnId(String id) {
+        driver.findElement(By.id(id)).click();
+    }
+
+    public static void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public void clickOnLinkText(String lnkTxt) {
+        driver.findElement(By.linkText(lnkTxt)).click();
+    }
+
+    public void clickOnXpath(String xpath) {
+        driver.findElement(By.xpath(xpath)).click();
+    }
+
+    public void typeOnXpath(String xpath, String data) {
+        driver.findElement(By.xpath(xpath)).sendKeys(data);
+    }
+
+
+}
